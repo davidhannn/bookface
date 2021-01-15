ff69be80cca7684f44c6fced252d7dbba5b24bb1
David Han <davidhannn@yahoo.com>
2021-01-14 19:34:43 -0800

    Updated notification functionality so that repeating a like on a post won't send multiple notifications

--- src/components/post/post.component.jsx
+++ src/components/post/post.component.jsx
@@ -23,102 +23,42 @@ const Post = ({ postId, currentUser, data: { userId, image, message, profilePic,
     const [comment, setComment] = useState("");
     const [postCommentList, setPostCommentList ] = useState([]);
     const [likeStatus, setLikeStatus] = useState("notLiked");
+    const [notificationStatus, setNotificationStatus] = useState("");
 
     const handleLike =  (e) => {
 
         e.preventDefault();
 
-        const postSnapshot = firestore.collection('postLikes').doc(postId).get();
+        const postLikesRef = firestore.collection('postLikes').doc(postId);
+        const postRef = firestore.collection('posts').doc(postId);
+
 
         if (likeStatus === "notLiked") { 
-            firestore.collection('postLikes').doc(postId).set({ [currentUser.id]: true}, {merge: true})
-            firestore.collection('posts').doc(postId).update({
+            postLikesRef.set({ [currentUser.id]: true}, {merge: true})
+            postRef.update({
                     likes: likes + 1
                 })
+
+            if(notificationStatus === "notSent") {
+                    firestore.collection('notifications').add({
+                    read: false,
+                    postId: postId,
+                    sender: currentUser.id,
+                    recipient: userId,
+                    type: "like",
+                    createdAt: firebase.firestore.FieldValue.serverTimestamp()
+                })
+                setNotificationStatus("sent")
+            }
+
             setLikeStatus("liked")
         } else {
-            firestore.collection('postLikes').doc(postId).set({ [currentUser.id]: false })
-            firestore.collection('posts').doc(postId).update({
+            postLikesRef.set({ [currentUser.id]: false })
+            postRef.update({
                 likes: likes - 1
             })
             setLikeStatus("notLiked")
         }
-
-
-        // if (likeStatus === "notLiked") { 
-        //     if(postSnapshot == undefined) {
-        //         firestore.collection('postLikes').doc(postId).set({ [currentUser.id]: true})
-        //         firestore.collection('posts').doc(postId).update({
-        //             likes: likes + 1
-        //         })
-        //     } else {
-        //         postSnapshot.then(doc => {
-        //             const userLikeStatus = doc.data()[currentUser.id]
-        //             if (userLikeStatus == undefined || userLikeStatus === false) {
-        //                 firestore.collection('postLikes').doc(postId).update({ [currentUser.id]: true})
-        //                 firestore.collection('posts').doc(postId).update({
-        //                     likes: likes + 1
-        //                 })
-        //             } 
-        //         })
-        //     }
-        //     setLikeStatus("liked")
-        // } else {
-        //     firestore.collection('postLikes').doc(postId).set({ [currentUser.id]: false })
-        //     firestore.collection('posts').doc(postId).update({
-        //         likes: likes - 1
-        //     })
-
-        //     setLikeStatus("notLiked")
-        // }
-
-        // const postSnapshot = firestore.collection('postLikes').doc(postId).get();
-
-        //     if(postSnapshot == undefined || !postSnapshot.exists) {
-        //         firestore.collection('postLikes').doc(postId).set({ [currentUser.id]: true })
-        //         firestore.collection('posts').doc(postId).update({
-        //             likes: likes + 1
-        //         })
-        //         firestore.collection('notifications').add({
-        //             read: false,
-        //             postId: postId,
-        //             sender: currentUser.id,
-        //             recipient: userId,
-        //             type: "like",
-        //             createdAt: firebase.firestore.FieldValue.serverTimestamp()
-        //         })
-        //     } else {
-        //         firestore.collection('postLikes').doc(postId).get().then(doc => {
-        //             const userLikeStatus = doc.data()[currentUser.id];
-        //             if (userLikeStatus === undefined) {
-        //                 firestore.collection('postLikes').doc(postId).update({ [currentUser.id]: true })
-        //                 firestore.collection('posts').doc(postId).update({
-        //                     likes: likes + 1
-        //                 })
-
-        //                 firestore.collection('notifications').add({
-        //                     read: false,
-        //                     postId: postId,
-        //                     sender: currentUser.id,
-        //                     recipient: userId,
-        //                     type: "like",
-        //                     createdAt: firebase.firestore.FieldValue.serverTimestamp()
-        //                 })
-        //             } else if (userLikeStatus === true) {
-        //                 firestore.collection('postLikes').doc(postId).update({ [currentUser.id]: false})
-        //                 firestore.collection('posts').doc(postId).update({
-        //                     likes: likes - 1
-        //                 })
-        //             } else if (userLikeStatus === false) {
-        //                 firestore.collection('postLikes').doc(postId).update({ [currentUser.id]: true })
-        //                 firestore.collection('posts').doc(postId).update({
-        //                     likes: likes + 1
-        //                 })
-        //             }
-        //         });
-                
-        //     }
-
 
     }
 
@@ -161,6 +101,7 @@ const Post = ({ postId, currentUser, data: { userId, image, message, profilePic,
         })
 
         firestore.collection('postLikes').doc(postId).get().then((doc) => {
+                if (doc.data() == undefined) return 
                 const userLikeState = doc.data()[currentUser.id]
                 if (userLikeState === true) {
                     setLikeStatus("liked")
@@ -169,7 +110,16 @@ const Post = ({ postId, currentUser, data: { userId, image, message, profilePic,
                 }
         })
 
-    }, [postCommentList, likeStatus])
+         firestore.collection('notifications').where('postId', '==', postId).where('recipient', "==", userId).where('sender', "==", currentUser.id).get().then(querySnapshot => {
+
+            if(querySnapshot.empty) {
+                setNotificationStatus("notSent")
+            } else {
+                setNotificationStatus("sent")
+            }
+        })
+
+    }, [postCommentList, likeStatus, notificationStatus])
   
     return (
         <div className="post" id={postId}>
