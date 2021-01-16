9e52c3d7722a0807bd1ac81b994aa9fb2cf54c72
David Han <davidhannn@yahoo.com>
2021-01-15 19:50:38 -0800

    updated friend request confirmation functionality in notifications

--- src/components/notification-dropdown-item/notification-dropdown-item.component.jsx
+++ src/components/notification-dropdown-item/notification-dropdown-item.component.jsx
@@ -10,62 +10,86 @@ import './notification-dropdown-item.styles.scss'
 const NotificationDropdownItem = ({ NotificationInfo }) => {
 
     const [senderData, setSenderData] = useState({});
-    const [friendRequestStatus, setFriendRequestStatus] = useState("");
+    const [friendRequestStatus, setFriendRequestStatus] = useState("pending");
     const history = useHistory();
 
     const { createdAt, postId, read, recipient, sender, type, status } = NotificationInfo;
 
     useEffect(() => {
             firestore.collection('users').doc(sender).get().then(doc => 
-                setSenderData(doc.data()))
+                setSenderData(doc.data())
+            )
+
+            firestore.collection('friendships').doc(sender).get().then(doc => {
+                const friendshipStatus = doc.data()[recipient];
+
+                if (friendshipStatus == true) {
+                    setFriendRequestStatus("approve")
+                } else {
+                    setFriendRequestStatus("deny")
+                }
+            })
+
     }, [friendRequestStatus])
 
 
     const { firstName, lastName, profileImgUrl } = senderData
     
+    console.log(friendRequestStatus)
 
-    const confirmOrDelete = () => {
-
-        const handleClick = (e) => {
-            if (e.target.value === "confirm") {
-                firestore.collection('friendships').doc(recipient).update({
-                    [sender]: true
-                })
-
-                firestore.collection('friendships').doc(sender).update({
-                    [recipient]: true
-                })
-
-                setFriendRequestStatus("approve")
-            } else if (e.target.value === "delete") {
-                firestore.collection('friendships').doc(recipient).update({
-                    [sender]: false
-                })
-
-                firestore.collection('friendships').doc(sender).update({
-                    [recipient]: false
-                })
-
-                setFriendRequestStatus("deny")
-            }
-
-        }
-
-        return (
-      
-            
-         <div className="friend-request">
-             <Avatar src={profileImgUrl} alt="" />   
-            <span><span style={{fontWeight: "bold"}}>{firstName} {lastName}</span> sent you a friend request</span>
-            <div className="button-row">
-                <button className="confirm-button" onClick={handleClick} value="confirm">Confirm</button>
-                <button className="delete-button" value="delete" onClick={handleClick}>Delete</button>
-            </div>
-        </div>
+    const handleClick = (e) => {
     
 
-        )
+        if (e.target.value === "confirm") {
+            firestore.collection('friendships').doc(recipient).update({
+                [sender]: true
+            })
+
+            firestore.collection('friendships').doc(sender).update({
+                [recipient]: true
+            })
+
+            setFriendRequestStatus("approve")
+        } else if (e.target.value === "delete") {
+            firestore.collection('friendships').doc(recipient).update({
+                [sender]: false
+            })
+
+            firestore.collection('friendships').doc(sender).update({
+                [recipient]: false
+            })
+
+            setFriendRequestStatus("deny")
+        }
+
     }
+
+    // const confirmOrDelete = () => {
+
+    //     return (
+      
+            
+    //      <div className="friend-request">
+    //          <Avatar src={profileImgUrl} alt="" />   
+             
+    //      {  friendRequestStatus == "pending" ? 
+    //         <div>
+    //             <span><span style={{fontWeight: "bold"}}>{firstName} {lastName}</span> sent you a friend request</span>
+    //             <div className="button-row">
+    //                 <button className="confirm-button" onClick={handleClick} value="confirm">Confirm</button>
+    //                 <button className="delete-button" value="delete" onClick={handleClick}>Delete</button>
+    //             </div>
+    //         </div>
+    //                 :
+    //         friendRequestStatus == "approve" ? <span>Friend Request Sent!</span> :
+    //         friendRequestStatus == "deny" ? <span>Friend Request Deleted</span> : 
+    //         null
+    //     }
+    //     </div>
+    
+
+    //     )
+    // }
 
     return (
         <li>
@@ -78,31 +102,36 @@ const NotificationDropdownItem = ({ NotificationInfo }) => {
                         </div>
                 </a> : 
             type == "comment" ?  
-            <Link to={`post/${postId}`} style={{ textDecoration: 'none'}}>
+            <a href="#" onClick={() => history.push(`/post/${postId}`)}>
 
                 <Avatar src={profileImgUrl} alt="" />      
                         <div className="notification-dropdown-item">
                                 <span style={{fontWeight: "bold"}}>{firstName} {lastName} &nbsp;commented your post</span>
                         </div>
-            </Link> : 
+            </a> : 
 
-            (type == "friendship" && status == "pending") ? confirmOrDelete() :
+            (type == "friendship" && status == "pending") ? 
+            <div className="friend-request">
+            <Avatar src={profileImgUrl} alt="" />   
+            
+            {  friendRequestStatus == "pending" ? 
+                    <div>
+                        <span><span style={{fontWeight: "bold"}}>{firstName} {lastName}</span> sent you a friend request</span>
+                        <div className="button-row">
+                            <button className="confirm-button" onClick={handleClick} value="confirm">Confirm</button>
+                            <button className="delete-button" value="delete" onClick={handleClick}>Delete</button>
+                        </div>
+                    </div>
+                            :
+                    friendRequestStatus == "approve" ? <span>Friend Request has been confirmed</span> :
+                    friendRequestStatus == "deny" ? <span>Friend Request has been deleted</span> : 
+                    null
+            }
+                </div>
+            :
             null }
         </li>
         
-        // <Link to={`post/${postId}`} style={{ textDecoration: 'none'}}>
-        //     <li>
-        //         <Avatar src={profileImgUrl} alt="" />      
-        //             <div className="notification-dropdown-item">
-        //                     <span style={{fontWeight: "bold"}}>{firstName} {lastName} </span>
-        //                         {type == "like" ? <span>&nbsp;liked your post</span> : 
-        //                         type == "comment" ? <span>&nbsp;commented on your post</span> : 
-        //                         (type == "friendship" && status == "pending") ? confirmOrDelete()
-        //                         : null}
-        //             </div> 
-        //             {/* <div className="notification-dropdown-item-time">{new Date(createdAt?.toDate()).toUTCString()}</div> */}
-        //     </li>
-        // </Link>
     )
 }
 
